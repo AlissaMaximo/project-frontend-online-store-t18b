56b5cc9baa8fb2db678b22d63d4521fe709295cb
José Vinicius <josevbluz@outlook.com>
2022-02-09 17:05:40 -0300

    requisito 3 feito

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Cart extends React.Component {
   render() {
     return (
-      <div>
+      <div className="containerCart">
         <h2 data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
         </h2>
