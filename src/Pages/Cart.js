21d56f9027c4172cd6dd3d027e4a89c25ee55620
C.Swame <caio_swame@hotmail.com>
2022-02-10 18:26:02 -0300

    requisito 9, criacao do storageAPI e add cart itens

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { getCartIten } from '../services/storageAPI';
 
 export default class Cart extends React.Component {
+  state = {
+    cartItensData: [],
+  }
+
+  componentDidMount() {
+    const cartItensData = getCartIten();
+    this.setState({ cartItensData });
+  }
+
+  showCartItens = () => {
+    const cartItensData = getCartIten();
+    return cartItensData.map(({
+      title,
+      price,
+      thumbnail,
+      id,
+      quantity,
+    }) => (
+      <div key={ id } className="containerCart">
+        <h3 data-testid="shopping-cart-product-name">{ title }</h3>
+        <img src={ thumbnail } alt={ `imagem do produto ${title}` } />
+        <p>{ price }</p>
+        <div data-testid="shopping-cart-product-quantity">{ quantity }</div>
+      </div>
+    ));
+  }
+
   render() {
+    const { cartItensData } = this.state;
+    if (cartItensData.length > 0) return this.showCartItens();
     return (
       <div className="containerCart">
         <h2 data-testid="shopping-cart-empty-message">
---
+++ src/services/storageAPI.js
@@ -0,0 +1,31 @@
+const CART_ITENS = 'cart_itens';
+
+if (!JSON.parse(localStorage.getItem(CART_ITENS))) {
+  localStorage.setItem(CART_ITENS, JSON.stringify([]));
+}
+const readCartIten = () => JSON.parse(localStorage.getItem(CART_ITENS));
+
+const saveCartIten = (CartIten) => localStorage
+  .setItem(CART_ITENS, JSON.stringify(CartIten));
+
+export const getCartIten = () => readCartIten();
+
+export const removeCartIten = (cartItem) => {
+  const cartItens = readCartIten();
+  saveCartIten(cartItens.filter((s) => s.id !== cartItem.id));
+};
+
+export const addCartIten = (cartData) => {
+  const cartItens = readCartIten();
+
+  const check = cartItens.some((iten) => iten.id === cartData.id);
+
+  if (check) {
+    const itenToRemove = cartItens.find((iten) => iten.id === cartData.id);
+    console.log(itenToRemove.id);
+    const newSavedItens = (cartItens.filter((s) => s.id !== cartData.id));
+    saveCartIten([...newSavedItens, cartData]);
+  } else if (cartData) {
+    saveCartIten([...cartItens, cartData]);
+  }
+};
